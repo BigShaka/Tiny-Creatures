cff03fb7bcd8fc1ba9f590d9ac623413e227e3b0
BautistaPereyra <166252719+BautistaPereyra@users.noreply.github.com>
2024-10-05 13:02:50 -0300

    Laser y Gliding
    (Estoy haciendo lo del gato y me quiero matar porque no me sale y se me rompe)

---
+++ Assets/Bau/Gliding.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gliding : MonoBehaviour
+{
+    [Header("Gliding")]
+    public KeyCode glideKey = KeyCode.LeftAlt;
+    public float glideSpeed = 5f;
+    public float glideDrag = 1f;
+    public float glideGravityScale = 0.5f;
+
+    private Rigidbody rb;
+    private bool isGliding;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(glideKey))
+        {
+            StartGliding();
+        }
+
+        if (Input.GetKeyUp(glideKey))
+        {
+            StopGliding();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (isGliding)
+        {
+            Glide();
+        }
+    }
+
+    private void StartGliding()
+    {
+        isGliding = true;
+        rb.drag = glideDrag;
+        rb.useGravity = false;
+    }
+
+    private void StopGliding()
+    {
+        isGliding = false;
+        rb.drag = 0;
+        rb.useGravity = true;
+    }
+
+    private void Glide()
+    {
+        Vector3 glideDirection = transform.forward * glideSpeed;
+        rb.AddForce(glideDirection, ForceMode.Acceleration);
+
+        // Simulacion de planecion
+        rb.AddForce(Vector3.down * glideGravityScale, ForceMode.Acceleration);
+    }
+}
---
+++ Assets/Bau/Laser.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    public Transform cameraPosition; // Referencia a la posición de la cámara
+    public Transform cameraOrientation; // Referencia a la orientación de la cámara
+    public GameObject laserPointer; // El objeto invisible que seguirá la punta del láser
+    public float laserRange = 100f;
+    public KeyCode laserKey = KeyCode.Mouse0; // Tecla para activar el láser
+
+    private LineRenderer laserLine;
+    private bool isLaserActive;
+
+    private void Start()
+    {
+        laserLine = GetComponent<LineRenderer>();
+        laserPointer.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(laserKey))
+        {
+            ActivateLaser();
+        }
+
+        if (Input.GetKeyUp(laserKey))
+        {
+            DeactivateLaser();
+        }
+
+        if (isLaserActive)
+        {
+            UpdateLaser();
+        }
+    }
+
+    private void ActivateLaser()
+    {
+        isLaserActive = true;
+        laserLine.enabled = true;
+        laserPointer.SetActive(true);
+    }
+
+    private void DeactivateLaser()
+    {
+        isLaserActive = false;
+        laserLine.enabled = false;
+        laserPointer.SetActive(false);
+    }
+
+    private void UpdateLaser()
+    {
+        Ray ray = new Ray(cameraPosition.position, cameraOrientation.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, laserRange))
+        {
+            laserLine.SetPosition(0, cameraPosition.position);
+            laserLine.SetPosition(1, hit.point);
+            laserPointer.transform.position = hit.point;
+        }
+        else
+        {
+            laserLine.SetPosition(0, cameraPosition.position);
+            laserLine.SetPosition(1, cameraPosition.position + cameraOrientation.forward * laserRange);
+            laserPointer.transform.position = cameraPosition.position + cameraOrientation.forward * laserRange;
+        }
+    }
+}
