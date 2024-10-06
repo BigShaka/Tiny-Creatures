35dff52cd22c35a52b5840cb004646b9a99cfb5a
Nacho <ignacio.bravob@davinci.edu.ar>
2024-10-06 16:45:25 -0300

    Player Set con camara

--- Assets/ScriptsNacho/POV_CAM.cs
+++ Assets/ScriptsNacho/POV_CAM.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class POV_CAM : MonoBehaviour
 {
@@ -8,6 +9,7 @@ public class POV_CAM : MonoBehaviour
     public float sensY;
 
     public Transform orientation;
+    public Transform camHolder;
 
     float _xAxis;
     float _yAxis;
@@ -27,8 +29,18 @@ public class POV_CAM : MonoBehaviour
         _xAxis -= mouseY;
         _xAxis = Mathf.Clamp(_xAxis, -90, 90);
 
-        transform.rotation = Quaternion.Euler(_xAxis, _yAxis, 0);
+        camHolder.rotation  = Quaternion.Euler(_xAxis, _yAxis, 0);
         orientation.rotation = Quaternion.Euler(0, _yAxis, 0);
 
+    }
+
+    public void DoFOV(float endValue)
+    {
+        GetComponent<Camera>().DOFieldOfView(endValue, 0.25f); 
+    }
+
+    public void DoTilt(float zTilt)
+    {
+        transform.DOLocalRotate(new Vector3(0, 0, zTilt), 0.25f);
     }
 }
